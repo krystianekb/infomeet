a9bee0e21d8f2bed0f36a1b3e8db9009a215005d
Krystian Brachmanski <krystian.brachmanski@gmail.com>
2015-06-19 18:41:43 +0200

    Change of the application wait

--- znode-watcher-example/src/main/java/org/infomeet/watcher/ApplicationLauncher.java
+++ znode-watcher-example/src/main/java/org/infomeet/watcher/ApplicationLauncher.java
@@ -1,5 +1,7 @@
 package org.infomeet.watcher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,15 +9,20 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("org.infomeet")
 public class ApplicationLauncher {
 
+	private static final Logger log = LoggerFactory.getLogger(ApplicationLauncher.class);
+	
+	private static void joinTheSubThread() {
+		   try {
+		        Thread.currentThread().join();
+		    } catch (InterruptedException e) {
+		        log.warn("Interrupted", e);
+		    }
+	}
+	
 	public static void main(String[] args) throws InterruptedException {
 		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(
 				"org.infomeet");
-		Object lock = new Object();
-        synchronized (lock) {
-            lock.wait();  
-        }
-        context.close();
-		
+		joinTheSubThread();		
 	}
 
 }
